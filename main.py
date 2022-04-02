ec7cb3d08f4c7f5d09745714eabe87f7bce45182
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-04-02 13:25:17 +0200

    modify to edit for '-test' in uri

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     calculator.convert_branches_to_keuzelijsten()
     differences = calculator.calculate_differences()
 
-    lines_to_write = [[f'Verschillen rapport gemaakt op {datetime.datetime.now()}'], ['omgeving','omschrijving verschil','uri object','uri keuzelijstwaarde (indien van toepassing)', 'waarde prd', 'waarde omgeving']]
+    lines_to_write = [[f'Verschillen rapport gemaakt op {datetime.datetime.now()}'], [], ['omgeving','omschrijving verschil','uri object','uri keuzelijstwaarde (indien van toepassing)', 'waarde prd', 'waarde omgeving']]
     lines_to_write.extend(differences)
 
     sheetsWrapper = SheetsWrapper(service_cred_path='C:\\resources\\driven-wonder-149715-ca8bdf010930.json',
